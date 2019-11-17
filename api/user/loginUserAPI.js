37db8b497dfd2a00ce903bd6990c7379c3373d93
Robert Rose <rwrose.projects@gmail.com>
2019-11-17 17:09:38 -0500

    Remove create-react-app dependencies

--- api/user/loginUserAPI.js
+++ api/user/loginUserAPI.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
     
     app.get('/api/loginUser', function(req, res) {
 
-        res.send({express: sess.name});
+        res.send({express: 'hello world'});
 
     });
 
