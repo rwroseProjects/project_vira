18945e479b14134df7890f57db1260028fde2508
Robert Rose <rwrose.projects@gmail.com>
2019-12-16 20:36:40 -0500

    Changes to make sign-in page better

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,9 +1,8 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import TextField from '@material-ui/core/TextField'
-import {Grid} from '@material-ui/core'
-import {Paper} from '@material-ui/core'
+import Login from '../login/Login'
+//import {Paper} from '@material-ui/core'
 
 class App extends Component {
 
@@ -13,25 +12,7 @@ class App extends Component {
 
 	render() {
 		return (
-			<div id="App_Encapsulation" style={{ padding: 20}}>
-				<Grid container 
-					direction="column" 
-					justify="center" 
-					spacing={1}
-					alignItems="center"
-					>
-					<Grid item>
-						<h1>Login to VIRA</h1>
-					</Grid>
-					<Grid item>
-						<TextField label="Username"></TextField>
-					</Grid>
-					<br/>
-					<Grid item>
-						<TextField label="Password"></TextField>
-					</Grid>
-				</Grid>
-			</div>
+			<Login />
 		)
 	}
 
